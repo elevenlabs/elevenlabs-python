6f88a2447b4e21c5dc57ca9be1822f949bce77ad
Flavio Schneider <flavio.schneider.97@gmail.com>
2023-05-04 21:23:38 +0200

    feat: python version >= 3.9

--- setup.py
+++ setup.py
@@ -19,6 +19,8 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
 )
