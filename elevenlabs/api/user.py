f7c732cc4ed567563162f0cec2a837479bb6bb76
Krishnan Navadia <krishnannavadia@gmail.com>
2023-05-31 11:53:41 +0530

    extended response in /user

--- elevenlabs/api/user.py
+++ elevenlabs/api/user.py
@@ -4,9 +4,18 @@ from .base import API, api_base_url_v1
 
 
 class Subscription(API):
+    status: str
+    tier: str
+    currency: str
     character_count: int
     character_limit: int
-    status: str
+    voice_limit: int
+    professional_voice_limit: int
+    allowed_to_extend_character_limit: bool
+    can_extend_character_limit: bool
+    can_extend_voice_limit: bool
+    can_use_instant_voice_cloning: bool
+    next_character_count_reset_unix: int
 
 
 class User(API):
