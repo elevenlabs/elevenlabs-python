b6af279e99d157558e07b09e8ad4f566301a50f5
Melanie Appleby <applebym12@gmail.com>
2024-01-16 22:29:35 -0500

    Added test cases from test_simple

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,35 +1,79 @@
 import pytest
 
-from elevenlabs import generate, voices, Voice, VoiceSettings, play, stream
+from .utils import no_api_key, repeat_test_without_api_key, use_play
+from elevenlabs import generate, voices, Voice, VoiceSettings, play, stream, get_api_key
 
+def test_api_key():
+    assert get_api_key() is not None
+    with no_api_key():
+        assert get_api_key() is None
+    assert get_api_key() is not None
 
-@pytest.mark.skip(reason="skip in ci")
-def test_voices() -> None:
-    print("Voices are...", voices())
+@repeat_test_without_api_key
+def test_voices():
+    # Test that we can get all voices
+    all_voices = voices()
+    assert len(all_voices) > 0
 
+@repeat_test_without_api_key
+def test_generate():
+    # Test that we can generate audio
+    audio = generate(text="Test voice generation.")
+    assert isinstance(audio, bytes) and len(audio) > 0
+    if use_play:
+        play(audio)
 
-@pytest.mark.skip(reason="skip in ci")
-def test_generate() -> None:
-    audio = generate(
-        text="Hello! My name is Bella.",
-        voice=Voice(
-            voice_id='EXAVITQu4vr4xnSDxMaL',
-            settings=VoiceSettings(stability=0.71, similarity_boost=0.5, style=0.0, use_speaker_boost=True)
-        )
-    )
-    play(audio)  # type: ignore
+def test_generate_stream():
+    from typing import Iterable
 
-@pytest.mark.skip(reason="skip in ci")
-def test_generate_stream() -> None:
-    def text_stream():
-        yield "Hi there, I'm Eleven "
-        yield "I'm a text to speech API "
+    # Test that we can generate audio stream
+    audio_stream = generate(text="Test voice streaming.", stream=True)
+    assert isinstance(audio_stream, Iterable)
 
+    if use_play:
+        audio = stream(audio_stream)
+        assert isinstance(audio, bytes) and len(audio) > 0
+
+def test_generate_stream_optimized():
+    from typing import Iterable
+
+    # Test that we can generate audio stream
     audio_stream = generate(
-        text=text_stream(),
-        voice="Nicole",
-        model="eleven_monolingual_v1",
-        stream=True
+        text="Test voice streaming optimized latency.", stream=True, latency=4
     )
+    assert isinstance(audio_stream, Iterable)
 
-    stream(audio_stream)  # type: ignore
+    if use_play:
+        audio = stream(audio_stream)
+        assert isinstance(audio, bytes) and len(audio) > 0
+
+def test_generate_with_voice():
+    # Test voice with name
+    audio = generate(text="Test voice with name.", voice="Rachel")
+    assert isinstance(audio, bytes) and len(audio) > 0
+    if use_play:
+        play(audio)
+
+    # Test voice with id
+    audio = generate(
+        text="Test voice with id.",
+        voice="21m00Tcm4TlvDq8ikWAM",
+    )
+    assert isinstance(audio, bytes) and len(audio) > 0
+    if use_play:
+        play(audio)
+
+    # Test voice with Voice object
+    voice = voices()[2]
+    audio = generate(text="Test voice with Voice object.", voice=voice)
+    assert isinstance(audio, bytes) and len(audio) > 0
+    if use_play:
+        play(audio)
+
+
+def test_generate_multilingual_v1():
+    # Test multilingual model
+    audio = generate(text="Prueba modelo multilingüe.", model="eleven_multilingual_v1")
+    assert isinstance(audio, bytes) and len(audio) > 0
+    if use_play:
+        play(audio)
