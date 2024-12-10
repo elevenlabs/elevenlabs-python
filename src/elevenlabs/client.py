973e80c01168df55725251a4ba3d4f81246b3ecd
Gal Bracha <galbra@gmail.com>
2024-12-10 12:14:08 +0200

    Added a function that returns the full dict instead of only the audio

--- src/elevenlabs/client.py
+++ src/elevenlabs/client.py
@@ -233,6 +233,90 @@ class ElevenLabs(BaseElevenLabs):
                 pronunciation_dictionary_locators=pronunciation_dictionary_locators
             )
 
+    def generate_stream_with_alignment(
+        self,
+        *,
+        text: Iterator[str],
+        voice: Union[VoiceId, VoiceName, Voice] = DEFAULT_VOICE,
+        voice_settings: typing.Optional[VoiceSettings] = DEFAULT_VOICE.settings,
+        model: Union[ModelId, Model] = "eleven_monolingual_v1",
+        optimize_streaming_latency: typing.Optional[int] = 0,
+        output_format: Optional[OutputFormat] = "mp3_44100_128",
+        pronunciation_dictionary_locators: typing.Optional[
+            typing.Sequence[PronunciationDictionaryVersionLocator]
+        ] = OMIT,
+        request_options: typing.Optional[RequestOptions] = None
+    ) -> Iterator[dict]:
+        """
+            - text: Iterator[str]. The stream of strings that will get converted into speech.
+
+            - voice: str. A voice id, name, or voice response. Defaults to the Sarah voice.
+
+            - model: typing.Optional[str]. Identifier of the model that will be used, you can query them using GET /v1/models.
+                                           The model needs to have support for text to speech, you can check this using the
+                                           can_do_text_to_speech property.
+
+            - optimize_streaming_latency: typing.Optional[int]. You can turn on latency optimizations at some cost of quality. The best possible final latency varies by model. Possible values:
+                                                                0 - default mode (no latency optimizations)
+                                                                1 - normal latency optimizations (about 50% of possible latency improvement of option 3)
+                                                                2 - strong latency optimizations (about 75% of possible latency improvement of option 3)
+                                                                3 - max latency optimizations
+                                                                4 - max latency optimizations, but also with text normalizer turned off for even more latency savings (best latency, but can mispronounce eg numbers and dates).
+
+                                                                Defaults to 0.
+
+            - output_format: typing.Optional[OutputFormat]. Output format of the generated audio. Must be one of:
+                                                   mp3_22050_32 - output format, mp3 with 22.05kHz sample rate at 32kbps.
+                                                   mp3_44100_32 - output format, mp3 with 44.1kHz sample rate at 32kbps.
+                                                   mp3_44100_64 - output format, mp3 with 44.1kHz sample rate at 64kbps.
+                                                   mp3_44100_96 - output format, mp3 with 44.1kHz sample rate at 96kbps.
+                                                   mp3_44100_128 - default output format, mp3 with 44.1kHz sample rate at 128kbps.
+                                                   mp3_44100_192 - output format, mp3 with 44.1kHz sample rate at 192kbps. Requires you to be subscribed to Creator tier or above.
+                                                   pcm_16000 - PCM format (S16LE) with 16kHz sample rate.
+                                                   pcm_22050 - PCM format (S16LE) with 22.05kHz sample rate.
+                                                   pcm_24000 - PCM format (S16LE) with 24kHz sample rate.
+                                                   pcm_44100 - PCM format (S16LE) with 44.1kHz sample rate. Requires you to be subscribed to Independent Publisher tier or above.
+                                                   ulaw_8000 - μ-law format (sometimes written mu-law, often approximated as u-law) with 8kHz sample rate. Note that this format is commonly used for Twilio audio inputs.
+
+                                                    Defaults to mp3_44100_128.
+
+            - voice_settings: typing.Optional[VoiceSettings]. Voice settings overriding stored setttings for the given voice. They are applied only on the given request.
+
+            - pronunciation_dictionary_locators: typing.Optional[typing.Sequence[PronunciationDictionaryVersionLocator]]. A list of pronunciation dictionary locators (id, version_id) to be applied to the text. They will be applied in order. You may have up to 3 locators per request
+
+            - request_options: typing.Optional[RequestOptions]. Request-specific configuration.
+        """
+        if isinstance(voice, str) and is_voice_id(voice):
+            voice_id = voice
+        elif isinstance(voice, str):
+            voices_response = self.voices.get_all(request_options=request_options, show_legacy=True)
+            maybe_voice_id = next((v.voice_id for v in voices_response.voices if v.name == voice), None)
+            if maybe_voice_id is None:
+                raise ApiError(body=f"Voice {voice} not found.")
+            voice_id = maybe_voice_id
+        elif isinstance(voice, Voice):
+            voice_id = voice.voice_id
+            if voice_settings == DEFAULT_VOICE.settings \
+                and voice.settings is not None:
+                voice_settings = voice.settings
+        else:
+            voice_id = DEFAULT_VOICE.voice_id
+
+        if isinstance(model, str):
+            model_id = model
+        elif isinstance(model, Model):
+            model_id = model.model_id
+
+
+        return self.text_to_speech.convert_realtime_full(  # type: ignore
+            voice_id=voice_id,
+            voice_settings=voice_settings,
+            output_format=output_format,
+            text=text,
+            request_options=request_options,
+            model_id=model_id
+        )
+
 
 class AsyncElevenLabs(AsyncBaseElevenLabs):
     """
--- src/elevenlabs/realtime_tts.py
+++ src/elevenlabs/realtime_tts.py
@@ -15,6 +15,7 @@ from .core.request_options import RequestOptions
 from .types.voice_settings import VoiceSettings
 from .text_to_speech.client import TextToSpeechClient
 from .types import OutputFormat
+from .text_to_speech.types.text_to_speech_stream_with_timestamps_response import TextToSpeechStreamWithTimestampsResponse
 
 # this is used as the default value for optional parameters
 OMIT = typing.cast(typing.Any, ...)
@@ -137,3 +138,100 @@ class RealtimeTextToSpeechClient(TextToSpeechClient):
                     raise ApiError(body=data, status_code=ce.code)
                 elif ce.code != 1000:
                     raise ApiError(body=ce.reason, status_code=ce.code)
+
+    def convert_realtime_full(
+        self,
+        voice_id: str,
+        *,
+        text: typing.Iterator[str],
+        model_id: typing.Optional[str] = OMIT,
+        output_format: typing.Optional[OutputFormat] = "mp3_44100_128",
+        voice_settings: typing.Optional[VoiceSettings] = OMIT,
+        request_options: typing.Optional[RequestOptions] = None,
+    ) -> typing.Iterator[TextToSpeechStreamWithTimestampsResponse]:
+        """
+        Converts text into speech using a voice of your choice and returns audio.
+
+        Parameters:
+            - voice_id: str. Voice ID to be used, you can use https://api.elevenlabs.io/v1/voices to list all the available voices.
+
+            - text: typing.Iterator[str]. The text that will get converted into speech.
+
+            - model_id: typing.Optional[str]. Identifier of the model that will be used, you can query them using GET /v1/models. The model needs to have support for text to speech, you can check this using the can_do_text_to_speech property.
+
+            - voice_settings: typing.Optional[VoiceSettings]. Voice settings overriding stored setttings for the given voice. They are applied only on the given request.
+
+            - request_options: typing.Optional[RequestOptions]. Request-specific configuration.
+        ---
+        from elevenlabs import PronunciationDictionaryVersionLocator, VoiceSettings
+        from elevenlabs.client import ElevenLabs
+
+        def get_text() -> typing.Iterator[str]:
+            yield "Hello, how are you?"
+            yield "I am fine, thank you."
+
+        client = ElevenLabs(
+            api_key="YOUR_API_KEY",
+        )
+        client.text_to_speech.convert_realtime(
+            voice_id="string",
+            text=get_text(),
+            model_id="string",
+            voice_settings=VoiceSettings(
+                stability=1.1,
+                similarity_boost=1.1,
+                style=1.1,
+                use_speaker_boost=True,
+            ),
+        )
+        """
+        with connect(
+            urllib.parse.urljoin(
+                "wss://api.elevenlabs.io/",
+                f"v1/text-to-speech/{jsonable_encoder(voice_id)}/stream-input?model_id={model_id}&output_format={output_format}"
+            ),
+            additional_headers=jsonable_encoder(
+                remove_none_from_dict(
+                    {
+                        **self._client_wrapper.get_headers(),
+                        **(request_options.get("additional_headers", {}) if request_options is not None else {}),
+                    }
+                )
+            )
+        ) as socket:
+            try:
+                socket.send(json.dumps(
+                    dict(
+                        text=" ",
+                        try_trigger_generation=True,
+                        voice_settings=voice_settings.dict() if voice_settings else None,
+                        generation_config=dict(
+                            chunk_length_schedule=[50],
+                        ),
+                    )
+                ))
+            except websockets.exceptions.ConnectionClosedError as ce:
+                raise ApiError(body=ce.reason, status_code=ce.code)
+
+            try:
+                for text_chunk in text_chunker(text):
+                    data = dict(text=text_chunk, try_trigger_generation=True)
+                    socket.send(json.dumps(data))
+                    try:
+                        data = json.loads(socket.recv(1e-4))
+                        yield data
+                    except TimeoutError:
+                        pass
+
+                socket.send(json.dumps(dict(text="")))
+
+                while True:
+
+                    data = json.loads(socket.recv())
+                    yield data
+            except websockets.exceptions.ConnectionClosed as ce:
+                if "message" in data:
+                    raise ApiError(body=data, status_code=ce.code)
+                elif ce.code != 1000:
+                    raise ApiError(body=ce.reason, status_code=ce.code)
+
