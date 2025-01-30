d08b9fcd96e96406a26b4be3aecc6ab064b7e770
SalmanAziz <salmanaziz@Salman-Aziz.local>
2025-01-30 17:13:36 +0500

    fix: system type validation for promptagent

--- src/elevenlabs/types/prompt_agent_tools_item.py
+++ src/elevenlabs/types/prompt_agent_tools_item.py
@@ -53,7 +53,7 @@ class PromptAgentToolsItem_System(UncheckedBaseModel):
     description: str
 
     if IS_PYDANTIC_V2:
-        model_config: typing.ClassVar[pydantic.ConfigDict] = pydantic.ConfigDict(extra="allow", frozen=True) 
+        model_config: typing.ClassVar[pydantic.ConfigDict] = pydantic.ConfigDict(extra="allow", frozen=True)  # type: ignore # Pydantic v2
     else:
 
         class Config:
